f3e89b0dd3056cdd17bfddb00c598ffc3bafd3c0
Trukhym Viktoriia <vtruhym@gmail.com>
2026-05-10 15:54:27 +0300

    Fix vite build issue

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import istanbul from 'vite-plugin-istanbul'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    istanbul({
-      cypress: true,
-      requireEnv: false
-    })
-  ],
-  server: {
-    port: 3000
-  }
+  plugins: []
 })
